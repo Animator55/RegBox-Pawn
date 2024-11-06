201fdff1ea5972ccd25cbfab76bd129024696d6d
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-11-06 17:01:40 -0300

    Update TableView.tsx

--- src/components/TableView.tsx
+++ src/components/TableView.tsx
@@ -1,11 +1,40 @@
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { TableEvents } from '../vite-env'
 
 type Props = {
-    current: any
+    current: TableEvents | undefined
+    setDisplay: Function
 }
 
-export default function TableView({current}: Props) {
+export default function TableView({current, setDisplay}: Props) {
+  const Header = ()=>{
+    if(!current) return
+    return <header>
+      <button onClick={()=>{setDisplay("list")}}>
+        <FontAwesomeIcon icon={faCaretLeft}/>
+      </button>
+      <h3>{current.name}</h3>
+      <p>{current.state}</p>
+    </header>
+  }
+  const List = ()=>{ /// fix structure before doing this one
+    return <ul>
+      {current?.products.map(el=>{
+        return <div key={Math.random()}>{el.name}</div>
+      })}
+    </ul>
+  }
+
+  const Bottom = ()=>{
+    return <section>
+
+    </section>
+  }
+
   return <section>
-    
+    <Header/>
+    <List/>
   </section>
 }
