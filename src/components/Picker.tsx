e1e7b6c1b10ac45494a8c3bb4fb8cc61e5a280f7
Animator55 <animator55developer@gmail.com>
2024-11-09 20:10:21 -0300

    added item picker and styles

--- src/components/Picker.tsx
+++ src/components/Picker.tsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import { Item, router } from '../vite-env'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCaretLeft, faCaretUp, faPlus, faSortAlphaDownAlt, faSortAlphaUpAlt, faSortAmountAsc, faSortAmountDesc } from '@fortawesome/free-solid-svg-icons'
 import { productsType } from '../defaults/products'
+import SearchBar from './def/Search'
+import OrderListPop from './pops/OrderListPop'
 
 type Props = {
     setPageMain: Function
-    prods:productsType
+    prods: productsType
 }
 
-export default function Picker({setPageMain, prods}: Props) {
+export default function Picker({ setPageMain, prods }: Props) {
     const [result, setResult] = React.useState<Item[][]>([[]])
     const [phase, setPhase] = React.useState<number>(0)
+    const [search, setSearch] = React.useState<string>("")
 
     const [page, setPage] = React.useState<string>("")
+    const [pop, setPop] = React.useState<"order" | undefined>(undefined)
 
-    const addPhase = ()=>{
+    const [selectedItem, setSelectedItem] = React.useState<string | undefined>(undefined)
+
+    const addPhase = () => {
         setResult([...result, []])
         setPhase(result.length)
     }
 
-    const ShowCommand = ()=>{
+    const ShowCommand = () => {
         return <section>
             <div>Comanda</div>
-            <hr/>
-            {result.length !== 0 && result.map((pha, i)=>{
+            <hr />
+            {result.length !== 0 && result.map((pha, i) => {
                 return <section key={Math.random()}>
-                    <label>Tiempo {i+1}</label>
+                    <label>Tiempo {i + 1}</label>
                     <ul>
-                        {pha.length !== 0 && pha.map(item=>{
+                        {pha.length !== 0 && pha.map(item => {
                             return <li key={Math.random()}>
                                 {item.name}
                             </li>
@@ -39,57 +45,119 @@ export default function Picker({setPageMain, prods}: Props) {
         </section>
     }
 
-    const Header = ()=>{
-        return <header>
-            <button onClick={()=>{setPageMain("main")}}>Cancel</button>
-            <button>Confirm</button>
+    const Header = () => {
+        return <header className='main-header picker'>
+            <button className='default-button' onClick={() => { setPageMain("main") }}>Cancel</button>
+            <button className='default-button'>Confirm</button>
         </header>
     }
 
-    const TypeSelector = ()=>{
+    const TypeSelector = () => {
         let phases = []
-        for(let i=0; i<result.length; i++){
+        for (let i = 0; i < result.length; i++) {
             phases.push(<button
                 key={Math.random()}
-                onClick={()=>{setPhase(i)}}
+                onClick={() => { setPhase(i) }}
+                className={phase === i ? "active" : ""}
             >
-                {i+1}
+                {i + 1}
             </button>)
         }
         phases.push(<button
             key={Math.random()}
-            onClick={()=>{addPhase()}}
+            onClick={() => { addPhase() }}
         >
-            <FontAwesomeIcon icon={faPlus}/>
+            <FontAwesomeIcon icon={faPlus} />
         </button>)
 
-        return <section>
+        return <section className='page type-selector'>
             <header>
                 {phases}
             </header>
-            {Object.keys(prods).map(type=>{
-                return <button
-                    key={Math.random()}
-                    onClick={()=>{setPage(type)}}
-                >
-                    {type}
-                </button>
-            })}
+            <ul>
+                {Object.keys(prods).map(type => {
+                    return <button
+                        key={Math.random()}
+                        onClick={() => { setPage(type) }}
+                    >
+                        {type}
+                    </button>
+                })}
+            </ul>
         </section>
     }
 
-    const ItemSelector = ()=>{
-        return <section></section>
+    const sortIcons: { [key: string]: any } = {
+        "abc-r": faSortAlphaDownAlt,
+        "def": faSortAmountDesc,
+        "abc": faSortAlphaUpAlt,
+        "def-r": faSortAmountAsc,
+    }
+
+    const orderOptions = ["abc", "abc-r", "def", "def-r"]
+    let sortValue = "def"
+
+    const confirmOrderList = () => {
+        console.log("confirm")
+        setPop(undefined)
+    }
+
+    const ItemSelector = () => {
+        if (page === "") return
+        return <section className='page'>
+            <div className='item-selector'>
+
+                <header className='table-list-header'>
+                    <SearchBar
+                        searchButton={setSearch}
+                        defaultValue={search}
+                        placeholder='Buscar...'
+                        onChange
+                    />
+                    <button className='default-button' onClick={() => {
+                        setPop("order")
+                    }}>
+                        <FontAwesomeIcon icon={sortIcons[sortValue]} />
+                    </button>
+                </header>
+                <ul>
+                    {prods[page].map(item => {
+                        return <button
+                            key={Math.random()}
+                            className={selectedItem === item._id ? "active" : ""}
+                            onClick={() => { setSelectedItem(item.id) }}
+                        >
+                            {item.name}
+                        </button>
+                    })}
+                </ul>
+            </div>
+            <div className='inspector'>
+                    
+            </div>
+
+            <button className='return-to-type-selector' onClick={() => { setPage("") }}>
+                <FontAwesomeIcon icon={faCaretLeft} />
+                Volver
+            </button>
+        </section>
     }
 
     const pages: router = {
-        "types": <TypeSelector/>,
-        "items": <ItemSelector/>,
+        "types": <TypeSelector />,
+        "items": <ItemSelector />,
     }
 
-    return <section>
-        <Header/>
+    return <>
+        {pop && <OrderListPop
+            options={orderOptions}
+            actual={"def"}
+            confirm={confirmOrderList}
+            close={() => { setPop(undefined) }}
+        />}
+        <Header />
         {pages[page !== "" ? "items" : "types"]}
-        <button onClick={()=>{console.log("a")}}>Ver comanda</button>
-    </section>
+        <button className='view-command-button' onClick={() => { console.log("a") }}>
+            <FontAwesomeIcon icon={faCaretUp} /> Ver comanda</button>
+    </>
 }
