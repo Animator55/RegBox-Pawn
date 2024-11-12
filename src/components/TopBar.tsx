c242f02fad1f8f83228e341b8c63292649c62fc1
Animator55 <animator55developer@gmail.com>
2024-11-12 19:07:53 -0300

    Update TopBar.tsx

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,30 +5,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleDollarToSlot, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { Item } from '../vite-env'
 
-type Props = {pickerOn: Item[][]}
+type Props = { pickerOn: Item[][] }
 
-export default function TopBar({pickerOn}: Props) {
-    const [pop, setPop] = React.useState<"products" | "account" |undefined>(undefined)
+export default function TopBar({ pickerOn }: Props) {
+    const [pop, setPop] = React.useState<"products" | "account" | undefined>(undefined)
 
-    const closePop = ()=>{
+    const closePop = () => {
         setPop(undefined)
     }
 
-    const pops: {[key:string]: any}={
-        "products" : <ProdPop close={closePop}/>,
-        "account" : <AccountPop close={closePop}/>,
+    const pops: { [key: string]: any } = {
+        "products": <ProdPop close={closePop} />,
+        "account": <AccountPop close={closePop} />,
     }
-  return <header className='main-header'>
-    {pop && pops[pop]}
-    <button
-        onClick={()=>{setPop("products")}}
-    >
-        <FontAwesomeIcon icon={faCircleDollarToSlot}/>
-    </button>
-    <button
-        onClick={()=>{setPop("account")}}
-    >
-        <FontAwesomeIcon icon={faUserCircle}/>
-    </button>
-  </header>
+    return <header className='main-header'>
+        {(pickerOn.length === 1 && pickerOn[0].length !== 0)?
+            <>
+                {pop && pops[pop]}
+                <button
+                    onClick={() => { setPop("products") }}
+                >
+                    <FontAwesomeIcon icon={faCircleDollarToSlot} />
+                </button>
+                <button
+                    onClick={() => { setPop("account") }}
+                >
+                    <FontAwesomeIcon icon={faUserCircle} />
+                </button>
+            </>
+            :
+            <p>Selecciona una mesa</p>
+        }
+    </header>
 }
